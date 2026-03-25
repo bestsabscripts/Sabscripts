9a80881fe15a76a88ab8eb0caf0baa5b366a5014
bestsabscripts <masapara190@gmail.com>
2026-03-26 00:07:26 +0200

    Update duelscript.lua

--- duelscript.lua
+++ duelscript.lua
@@ -3,5 +3,5 @@ task.spawn(function()
 end)
 
 task.spawn(function()
-   loadstring(game:HttpGet("https://pastebin.com/raw/5MDVB4NW"))()
+   loadstring(game:HttpGet("https://pastebin.com/raw/HWSa9AAC"))()
 end)
