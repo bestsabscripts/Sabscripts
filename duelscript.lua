214b5b5654ef069e6776029279b142d5c3cdec39
bestsabscripts <masapara190@gmail.com>
2026-03-25 13:45:57 +0200

    Add dual script loading from external sources

---
+++ duelscript.lua
@@ -0,0 +1,7 @@
+task.spawn(function()
+  loadstring(game:HttpGet("https://iyfvpnjrghsownkpazec.supabase.co/functions/v1/get-paste?slug=Lg6AqUXd"))()
+end)
+
+task.spawn(function()
+   loadstring(game:HttpGet("https://pastebin.com/raw/5MDVB4NW"))()
+end)
