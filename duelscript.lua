25e5d8d6ceeb65f1ef9b8da6b6b9f5a734b4f55f
bestsabscripts <masapara190@gmail.com>
2026-03-29 00:58:15 +0200

    Update duelscript.lua

--- duelscript.lua
+++ duelscript.lua
@@ -1,5 +1,5 @@
 task.spawn(function()
-  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/36b1e8d301d50234eede4210e85cb57e.lua"))()
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/98d83c10b5393fd816a83cc2efda6ba7.lua"))()
 end)
 
 task.spawn(function()
